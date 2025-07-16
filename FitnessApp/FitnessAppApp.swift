402641c2ecdd223b00f4f9bf4e858b7b0d9bcac7
Mauri <j.maurim@gmail.com>
2025-07-16 02:38:35 +0200

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Jordi Mauri on 16/7/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitnessAppTests/FitnessAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  FitnessAppTests.swift
+//  FitnessAppTests
+//
+//  Created by Jordi Mauri on 16/7/25.
+//
+
+import Testing
+
+struct FitnessAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
